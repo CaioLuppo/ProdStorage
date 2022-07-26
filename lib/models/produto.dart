dde98c8ab6ecba1befe6f812f942c87b559931b6
CaioLuppo <luppocode@outlook.com>
2022-07-26 16:22:13 -0300

    Adicionando modelo de produto

---
+++ lib/models/produto.dart
@@ -0,0 +1,15 @@
+// Modelo para o produto
+
+class Produto {
+  // Variáveis
+  final String nome;
+  final int quantidade;
+  final double valor;
+
+  // Construtor
+  Produto(
+      this.nome,
+      this.quantidade,
+      this.valor,
+      );
+}
