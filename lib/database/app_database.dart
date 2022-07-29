20fdfebeabdbfc79d786d45d2bed3235d603941c
CaioLuppo <luppocode@outlook.com>
2022-07-29 14:47:56 -0300

    Terminando banco de dados

---
+++ lib/database/app_database.dart
@@ -0,0 +1,27 @@
+// SQLite
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+// DAO
+import './dao/contact_dao.dart';
+
+
+Future<Database> getDB() async {
+
+  // Criação do caminho da database
+  final String path = join(await getDatabasesPath(), "inventario.db");
+
+  // Comunicação e criação do banco de dados
+  return openDatabase(
+    path,  // Passa o caminho do banco, depois as propriedades que quer mexer
+
+    onCreate: (
+        inventario,  // Banco
+        version,  // Versão
+        ) {
+      inventario.execute(Contact.createTable);
+    },
+
+    version: 1,  // Versão do banco que está utilizando
+  );
+}
